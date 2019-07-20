8011caba6a0f351ec6ddc18860989117ae3e7024
Jordan Paz <jordandavidpaz@gmail.com>
2019-07-20 11:31:18 -0700

    Add components folder. Add Priority component and PrioritiesPage component. Add styled components to package.json

---
+++ src/components/Priority.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Priority = props => {
+  return (
+    <div>
+      <h2>Priority</h2>
+    </div>
+  );
+};
+
+Priority.propTypes = {};
+
+export default Priority;
