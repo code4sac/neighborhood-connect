72715d51aa09aae677577ae1575bb5849c8d47ab
Eddie Chu <contact@eddiechu.me>
2019-07-20 19:49:04 -0700

    removed styled component from PrioritiesPage

--- src/components/PrioritiesPage.js
+++ src/components/PrioritiesPage.js
@@ -1,27 +1,12 @@
 import React from "react";
 import PriorityCard from "./PriorityCard";
-import styled from "styled-components";
-
-const PrioritiesWrapper = styled.div`
-  margin-bottom: 1rem;
-`;
-
-const PriorityList = styled.ul`
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-`;
-
-const StyledTitle = styled.h1`
-  font-size: 4rem;
-`;
 
 const PrioritiesPage = () => {
   return (
     <div>
-      <StyledTitle>Priorities</StyledTitle>
-      <PrioritiesWrapper>
-        <PriorityList>
+      <h1>Priorities</h1>
+      <div>
+        <ul>
           <li>
             <PriorityCard
               rank={1}
@@ -50,8 +35,8 @@ const PrioritiesPage = () => {
               description={"blah blah blah"}
             />
           </li>
-        </PriorityList>
-      </PrioritiesWrapper>
+        </ul>
+      </div>
     </div>
   );
 };
