176b1c7cff2464ee0401ccbccb84ecc63dd59f01
Eddie Chu <contact@eddiechu.me>
2019-07-20 19:19:14 -0700

    cleaning up and commenting code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
 import PriorityDetails from "./components/PriorityDetails";
 import PrioritiesPage from "./components/PrioritiesPage";
 import NewEventForm from "./components/NewEventForm";
