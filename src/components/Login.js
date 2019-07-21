c3cd77063d3c06dd9ae75db4fa8fb3894cb746ac
Ryan Doyle <doylecodes@gmail.com>
2019-07-21 16:12:02 -0700

    refactor Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const Login = () => {
-  return (
-    <div>
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
       <form action='/' method='POST'>
          <label htmlFor='email'>Email</label>
-         <input type='email' name='email' />
+         <input type='email' name='email' value={this.state.email} onChange={this.saveToState}/>
          <label htmlFor='password'>Email</label>
-         <input type='password' name='password' />
+         <input type='password' name='password' value={this.state.password} onChange={this.saveToState}/>
          <input type='submit' value='Login!'></input>
       </form>
     </div>
-  );
-};
-
-export default Login;
+    )
+  }
+}
