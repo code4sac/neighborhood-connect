3bc24bcc0454afa3274b8c498f6ac07daf6fc946
Ryan <ryandoyle@me.com>
2019-07-23 15:00:26 -0700

    added buttons to PriorityCard when at edit page, changed 'title' to 'type'

--- src/components/PriorityCard.js
+++ src/components/PriorityCard.js
@@ -2,21 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import blackArrow from '../assets/chevron-right-black.svg';
 
-const PriorityCard = ({ type, rank, description }) => {
+const PriorityCard = ({ rank, type, description, promote, demote, location }) => {
   return (
+    <div className="priorityCard">
+      <div className="priorityCard__banner">
+        <p>#{rank} Priority</p>
+        <p>{location}</p>
+      </div>
+      <div className="priorityCard__overview">
+        <h2 className="heading-secondary">{type}</h2>
+        <p className="descriptions">{description}</p>
+      </div>
+      {(location === '/editPriorities') && (
+        <div style={{ margin: '1rem' }}>
+          <button onClick={() => { promote() }} style={{ height: '15px', width: '60px' }}>^</button>
+          <button onClick={() => { demote() }} style={{ height: '15px', width: '60px' }}>V</button>
+        </div>
+      )}
       <Link to="/priorityDetails">
-          <div className="priorityCard">
-              <div className="priorityCard__banner">
-                  <p>#{rank} Priority</p>
-                  <p>{type}</p>
-              </div>
-              <div className="priorityCard__overview">
-                  <h2 className="heading-secondary">{type}</h2>
-                  <p className="descriptions">{description}</p>
-              </div>
-              <img className="priorityCard__image chevronArrow" src={blackArrow} alt="arrow" />
-          </div>
+        <img className="priorityCard__image chevronArrow" src={blackArrow} alt="arrow" />
       </Link>
+    </div>
   );
 };
 
