785f73785fc45664f81d9c5d0188e2ef92365a48
Ryan <ryandoyle@me.com>
2019-07-24 22:22:28 -0700

    Make header only render img when passed as prop from parent

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 export default class Header extends React.Component {
-    render() {
-        return (
-            <header className="header">
-                <h1 className="heading-primary u-margin-left-small">{this.props.title}</h1>
-                <Link className="btn btn--aligned" to={this.props.option}><span className="btn__text"><img src={this.props.optionIcon} alt={this.props.optionName} /> {this.props.optionName}</span></Link>
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header className="header">
+        <h1 className="heading-primary u-margin-left-small">{this.props.title}</h1>
+        <Link className="btn btn--aligned" to={this.props.option}>
+          <span className="btn__text">
+            {/* Won't render img icon unless passed the prop in parent page (for different views) */}
+            {this.props.optionIcon && <img src={this.props.optionIcon} alt={this.props.optionName} />}
+            {this.props.optionName}
+          </span>
+        </Link>
+      </header>
+    )
+  }
 }
