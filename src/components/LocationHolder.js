d5bc050ab330ab6dedf747c089da10a3cba3c7ab
Eddie Chu <contact@eddiechu.me>
2019-07-25 00:12:05 -0700

    use ternary for conditional render

--- src/components/LocationHolder.js
+++ src/components/LocationHolder.js
@@ -3,19 +3,11 @@ import whiteArrow from '../assets/chevron-right-white.svg'
 import { Link } from 'react-router-dom';
 
 export default class LocationHolder extends React.Component {
+ 
     render() {
-
-        console.log(this.props.hood)
-
-        let currentNeighborhood = this.props.hood ? (
-            <Link to="selectNeighborhood" className="locationHolder__current btn btn--primary-blue">
-                {this.props.hood}
-            </Link>
-        ) : (
-            <Link to="selectNeighborhood" style={{ visibility: "hidden" }} className="locationHolder__current btn btn--primary-blue">
-                {this.props.hood}
-            </Link>
-        );
+        
+        let visibility;
+        this.props.hood ? visibility = "visible" : visibility = "hidden";
 
         return (
             <div className="locationHolder">
@@ -27,7 +19,9 @@ export default class LocationHolder extends React.Component {
                 <div className="seperator" />
                 {/* This.props.hood is rendering as just neighborhood? shouldn't it be receiving the name of the saved neighborhood as props?? */}
                 {/* I am just linkeding to the selectNeighborhood component for now, since I'm not sure if clicking it is supposed to be bring us to that neighborhood specifically */}
-                {currentNeighborhood} 
+                <Link to="selectNeighborhood" style={{ visibility: `${visibility}` }} className="locationHolder__current btn btn--primary-blue">
+                    {this.props.hood}
+                </Link>
             </div>
         );
     }
