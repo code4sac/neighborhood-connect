3bf01084d0e1a84365a8f307d614db76a1ccb5e2
Eddie Chu <contact@eddiechu.me>
2019-07-25 22:38:33 -0700

    tentative styling on NewActionForm

--- src/components/NewActionForm.js
+++ src/components/NewActionForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 export default class NewEventForm extends Component {
   state = {
@@ -15,59 +16,51 @@ export default class NewEventForm extends Component {
 
   render() {
     return (
-      <>
-        <h2>Add a New Event</h2>
-        <p>What's up in the community?</p>
-        <div>
-          <form
-            name='new-event'
-            onSubmit={async e => {
-              e.preventDefault();
-              fetch('INSERT_POST_URL', {
-                method: 'POST',
-                body: JSON.stringify(this.state)
-              }
-              )
-                .then(this.setState({ name: '', type: '', date: '', goal: '', details: '' }))
-            }}>
-            <label htmlFor='name'>Event Name</label>
-            <input
-              type='text'
-              name='name'
-              value={this.state.name}
-              onChange={this.saveToState} />
+        <div className="actionspage">
+            <Header title={"Add New Event"} />
+            <div>
+                <form
+                    className="form"
+                    name="new-event"
+                    //Consider refactoring this POST to a method outside of the form tag
+                    onSubmit={async e => {
+                        e.preventDefault();
+                        fetch("INSERT_POST_URL", {
+                            method: "POST",
+                            body: JSON.stringify(this.state)
+                        }).then(this.setState({ name: "", type: "", date: "", goal: "", details: "" }));
+                    }}
+                >
+                    <label className="form__label" htmlFor="name">
+                        Event Name
+                    </label>
+                    <input className="form__input" type="text" name="name" value={this.state.name} onChange={this.saveToState} />
 
-            <label htmlFor='type'>What Type of Event?</label>
-            <input
-              type='text'
-              name='type'
-              value={this.state.type}
-              onChange={this.saveToState} />
+                    <label className="form__label" htmlFor="type">
+                        What Type of Event?
+                    </label>
+                    <input className="form__input" type="text" name="type" value={this.state.type} onChange={this.saveToState} />
 
-            <label htmlFor='date'>When?</label>
-            <input
-              type='date'
-              name='date'
-              value={this.state.date}
-              onChange={this.saveToState} />
+                    <label className="form__label" htmlFor="date">
+                        When?
+                    </label>
+                    <input className="form__input" type="date" name="date" value={this.state.date} onChange={this.saveToState} />
 
-            <label>Event Goal</label>
-            <input
-              type='text'
-              name='goal'
-              value={this.state.goal}
-              onChange={this.saveToState} />
+                    <label className="form__label" htmlFor="goal">
+                        Event Goal
+                    </label>
+                    <input className="form__input" type="text" name="goal" value={this.state.goal} onChange={this.saveToState} />
 
-            <label htmlFor='details'>Event Details</label>
-            <textarea type='text'
-              name='details'
-              value={this.state.details}
-              onChange={this.saveToState}
-            />
-            <button type='submit'>Create Event</button>
-          </form>
+                    <label className="form__label" htmlFor="details">
+                        Event Details
+                    </label>
+                    <textarea className="form__textarea" type="text" name="details" value={this.state.details} onChange={this.saveToState} />
+                    <button className="form__btn btn btn--secondary-blue u-margin-top-small" type="submit">
+                        Create Event
+                    </button>
+                </form>
+            </div>
         </div>
-      </>
-    )
+    );
   }
 }
