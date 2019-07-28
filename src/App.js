b67e866cb5178459e9737155589fd4faa0e1daf4
wizard-jS <spraguej624@gmail.com>
2019-07-28 16:15:42 -0700

    Line 62. Change string to number

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import PriorityDetails from "./components/PriorityDetails";
-import PrioritiesPage from "./components/PrioritiesPage";
-import NeighborhoodSelector from "./components/NeighborhoodSelector";
-import NewPriorityForm from "./components/NewPriorityForm";
+import PriorityDetails from './components/PriorityDetails';
+import PrioritiesPage from './components/PrioritiesPage';
+import NeighborhoodSelector from './components/NeighborhoodSelector';
+import NewPriorityForm from './components/NewPriorityForm';
 import PrioritiesOrderPage from './components/PrioritiesOrderPage';
 import Login from './components/Login';
-import CreateAccount from "./components/CreateAccount";
-import ActionsPage from "./components/ActionsPage";
+import CreateAccount from './components/CreateAccount';
+import ActionsPage from './components/ActionsPage';
 import NewActionForm from './components/NewActionForm';
-
 
 function App() {
   const [neighborhood, setNeighborhood] = useState(() => {
-    const initialName = localStorage.getItem('neighborhoodName')
+    const initialName = localStorage.getItem('neighborhoodName');
     return initialName;
-  })
+  });
 
   const [orgId, setOrgId] = useState(() => {
     const initialId = localStorage.getItem('orgId');
@@ -28,22 +27,24 @@ function App() {
       <Route
         exact
         path="/"
-        render={props => <PrioritiesPage
-          orgId={orgId}
-          setOrgId={setOrgId}
-          neighborhood={neighborhood}
-          {...props}
-        />}
+        render={(props) => (
+          <PrioritiesPage
+            orgId={orgId}
+            setOrgId={setOrgId}
+            neighborhood={neighborhood}
+            {...props}
+          />
+        )}
       />
       <Route
         exact
         path="/priorityDetails"
-        render={props => <PriorityDetails {...props} />}
+        render={(props) => <PriorityDetails {...props} />}
       />
       <Route
         exact
         path="/selectNeighborhood"
-        render={props => (
+        render={(props) => (
           <NeighborhoodSelector
             neighborhood={neighborhood}
             setNeighborhood={setNeighborhood}
@@ -55,16 +56,17 @@ function App() {
       <Route
         exact
         path="/editPriorities"
-        render={props => <PrioritiesOrderPage
-          neighborhood={neighborhood}
-          orgId={orgId}
-          {...props} />}
+        render={(props) => (
+          <PrioritiesOrderPage
+            neighborhood={neighborhood}
+            orgId={Number(orgId)}
+            {...props}
+          />
+        )}
       />
       <Route
         path="/actions/:priorityId" // change later to some regex to match 'priority-asd' or something similar
-        render={props => <ActionsPage
-          {...props} />
-        }
+        render={(props) => <ActionsPage {...props} />}
       />
       <Route exact path="/addNewPriority" component={NewPriorityForm} />
       <Route exact path="/login" component={Login} />
